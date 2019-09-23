f3a4d0e6a8b4ede9c39ee8de01b4ab50368a11ee
z1235789 <53037944+z1235789@users.noreply.github.com>
2019-09-23 16:13:24 +0200

    Update and rename Original_sample.m to Random_ring.m

--- Random_ring.m
+++ Random_ring.m
@@ -1,21 +1,26 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Generierung von mehreren Spots nach der Linsen und Prismen Methode
+% Erzeugung mehrerer Fallen mit dreidimensionaler Anordnung m鰃lich
+% Algorithmus implementiert nach Liesener 2000
+% September 2015, Jannis K鰄ler
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear all;
 clc;
 close all
 
-% x_res = 1920; % Auflösung des SLM eingeben
+% x_res = 1920; % Aufl鰏ung des SLM eingeben
 % y_res = 1080;
-x_res = 768; % Selbst Size des Aufloesung eingeben
-y_res = 768;
-
+x_res = 1080; % Aufl鰏ung des SLM eingeben
+y_res = 1080;
 [X,Y] = meshgrid(1:1:x_res, 1:1:y_res);
 [X2,Y2] = meshgrid(-x_res/2:1:x_res/2-1, -y_res/2:1:y_res/2-1);
 
 % Spot 1:
 shift_x1 = 0; % Auslenkung in x-Richtung, max. Sichtbereich auf CMOS: x = 
 shift_y1 = 0; % Auslenkung in y-Richtung, max. Sichtbereich auf CMOS: y = 
-shift_z1 = -0.0001; % Verschiebung in z-Richtung (Positiv: verkürzter Fokus, Negativ: verlängerter Fokus); Achtung: Änderung sehr groß!
+shift_z1 = 0.00001; % Verschiebung in z-Richtung (Positiv: verk黵zter Fokus, Negativ: verl鋘gerter Fokus); Achtung: 膎derung sehr gro?!
 
-phi1 = mod(1/100 * (2*pi*shift_x1*X + 2*pi*shift_y1*Y)+shift_z1*(X2.^2+Y2.^2),2*pi); %mod(a,m),m??a????
+phi1 = mod(1/100 * (2*pi*shift_x1*X + 2*pi*shift_y1*Y)+shift_z1*(X2.^2+Y2.^2),2*pi);
 
 % Spot 2:
 shift_x2 = 0;
@@ -24,23 +29,18 @@ shift_z2 = 0;
 
 phi2 = mod(1/100 * (2*pi*shift_x2*X + 2*pi*shift_y2*Y)+shift_z2*(X2.^2+Y2.^2),2*pi);
 
-% Für den erhalt der 0ten-Ordung: Spot mit x/y/z=0 (ergibt bei der Superposition +1)
+% F黵 den erhalt der 0ten-Ordung: Spot mit x/y/z=0 (ergibt bei der Superposition +1)
 
-%Random Image:option
-phi_random = rand([1080 768]);
+%Random Image:
+phi_random = rand([1080 1920]);
 maxv=max(max(phi_random));
 minv=min(min(phi_random));
 max_soll=2*pi; %Nach SLM Gamma Curve: max 166
 min_soll=0;
 As=((phi_random-minv)/(maxv-minv)*(max_soll-min_soll))+min_soll;
-As = As.';
 
 % Superposition der einzelnen Spots:
-A = exp(1i.*phi1);
-B = exp(1i.*phi2);
-C = exp(1i.*As);    %random noise
-superpos = A+B;%+C/1000;
-% superpos = exp(1i.*phi1) + exp(1i*phi2);% + exp(1i.*As); 
+superpos = exp(1i.*phi1) + exp(1i*phi2);% + exp(1i.*As); 
 % Ermittlung der Phase:
 phi_sp = angle(superpos); % phi_superposition
 
@@ -49,16 +49,13 @@ maxv=max(max(phi_sp));
 minv=min(min(phi_sp));
 max_soll=255; %Nach SLM Gamma Curve
 min_soll=0;
-Q = phi_sp-minv;
-W = maxv-minv;
-E = max_soll-min_soll;
-phi_sp_gv=uint8(((Q)/(W)*(E))+min_soll); %grey value
+phi_sp_gv=uint8(((phi_sp-minv)/(maxv-minv)*(max_soll-min_soll))+min_soll); %grey value
 
 % Phasenfunktion anzeigen auf dem SLM (2. Bildschirm):
-figure(6)
+figure()
 % set(gcf,'outerposition', [1913         113        1936     1118]); % Verschiebung auf den Bildschirm des SLM
 % set(gca,'Position', [0 0 1 1]);
 set(gca,'Visible', 'Off');
 set(gcf,'menubar','none');
 imshow(phi_sp_gv);
-imwrite(phi_sp_gv,'Ring_0.jpg');    %Speichen das Bild fuer weitere Anwendung
+imwrite(phi_sp_gv,'ring.jpg');
