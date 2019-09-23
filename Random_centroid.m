bc12252a7457097e125cac15576938562fd91d32
z1235789 <53037944+z1235789@users.noreply.github.com>
2019-09-23 16:11:14 +0200

    Update and rename Centroid.m to Random_centroid.m
    Change the name

--- Random_centroid.m
+++ Random_centroid.m
@@ -1,4 +1,4 @@
-function centroid_x_y =centroid()    % The first function to run,returning the center value of the beam-spot
+function centroid_x_y = centroid_x_y()    % The first function to run,returning the center value of the beam-spot
 vid = videoinput('winvideo',2);    % Use the camera's default value
 set(vid,'ReturnedColorSpace','Grayscale');     % Use the grayscale
 set(vid,'TriggerRepeat',Inf);
@@ -11,9 +11,10 @@ BW = im2bw(frame);    % This is a command be used in Matlab2015,in 2019 we use f
 BW = bwconncomp(BW,8);
 cent = regionprops(BW,'Centroid');
 nspots = length(cent);
-centre = zeros(nspots,1);
+centre = zeros(nspots,2);
 centre(1,:) = cent(1).Centroid; 
 
-centroid_x_y = [centre(1),centre(2)];    % centroid_x_y is a 1*1 matrix,it's the return of this function
+centroid_x_y = [centre(1,1),centre(1,2)];
+%centroid_x_y = [centre(1),centre(2)];    % centroid_x_y is a 1*1 matrix,it's the return of this function
 
 end
