f4668fdbfa6b07b01aa0267e7a0bcc104eb4f7ce
z1235789 <53037944+z1235789@users.noreply.github.com>
2019-09-25 16:18:05 +0200

    Update Random_main.m
    Useful

--- Random_main.m
+++ Random_main.m
@@ -10,17 +10,18 @@ min = 0;
 mean = (max+min)/2;
 
 Bild = zeros(m,n);    %a black image
-imwrite(Bild,strcat('a\','black','.jpg'),'jpg');
+imwrite(Bild,strcat('E:\Data\a\','black','.jpg'),'jpg');
 
 % Centroid determinate
-centre_x_y = centroid_x_y();
+centre_x_y = Random_centroid_x_y();
 centre_x = centre_x_y(1);
 centre_y = centre_x_y(2);
 
 % main function
 tic;
-for i = 50:50:250
-    for j = 50:50:400   %50-700行，50--1000列
+
+for i = 50:50:1050
+    for j = 50:50:700   %50-700ÐÐ£¬50--1000ÁÐ
         Random_spots(Bild,i,j,n,m,centre_x,centre_y);
             
         Bild = zeros(m,n);
@@ -28,4 +29,6 @@ for i = 50:50:250
 end
         
 toc;
+
+
 
